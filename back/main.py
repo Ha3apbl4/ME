b8e011104a30e5a096d3725a24236e712eed9f88
Nazar <nazarpochtar984@gmail.com>
2026-03-05 11:49:23 +0200

    Перероблений запуск FastAPI, додана логіка видалення з бази данних двигунів

--- back/main.py
+++ back/main.py
@@ -4,6 +4,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
+import uvicorn
 import os
 
 from api.add_motors import add as add_motor_router
@@ -37,7 +38,7 @@ async def root(request: Request):
     cur.execute("SELECT * FROM motors_list ORDER BY id ASC LIMIT 5;")
     motors = cur.fetchall()
 
-    cur.close()
+    cur.close() 
 
     motors_dict =[
         {
@@ -53,7 +54,31 @@ async def root(request: Request):
     return templates.TemplateResponse(
         "index.html",{"request": request, 'motors': motors_dict})
 
+    
+@app.delete('/delete_motor/{motor_id}')
+async def DeleteMotor(motor_id: int):
+    cursor = conn.cursor()
 
+    cursor.execute('DELETE FROM motors_list WHERE id = %s', (motor_id,))
+    conn.commit()
+    cursor.close()
+    
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT COUNT(*) FROM motors_list;")
+    count = cursor.fetchone()[0]
+
+    if count == 0:
+        # Таблиця порожня → скидаємо sequence
+        cursor.execute("ALTER SEQUENCE motors_list_id_seq RESTART WITH 1;")
+        conn.commit()
+
+    cursor.close()
+    return{ 'message': 'Двигуна {motor_id} видалено '}
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", reload=True)
 
 
    
